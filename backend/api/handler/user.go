dab16c476c280c97d3b07e3a1490116984e94849
aldisatria12 <aldisatria253@gmail.com>
2024-12-14 14:26:53 +0700

    changes feat: user login handler feat: routes setup

---
+++ backend/api/handler/user.go
@@ -0,0 +1,55 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/aldisatria12/terradiscover/dto"
+	"github.com/aldisatria12/terradiscover/service"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+type UserHandler struct {
+	userService service.UserService
+}
+
+func NewUserHandler(s service.UserService) UserHandler {
+	return UserHandler{
+		userService: s,
+	}
+}
+
+func (h UserHandler) Login(c *gin.Context) {
+	var logUser dto.UserLoginRequest
+	err := c.ShouldBindJSON(&logUser)
+
+	if err != nil {
+		c.Error(err)
+
+		return
+	}
+
+	validate := validator.New()
+	err = validate.Struct(logUser)
+
+	if err != nil {
+		c.Error(err)
+
+		return
+	}
+
+	user, err := h.userService.Login(c, logUser)
+
+	if err != nil {
+		c.Error(err)
+
+		return
+	}
+
+	response := gin.H{
+		"Msg":  "OK",
+		"Data": user,
+	}
+
+	c.JSON(http.StatusOK, response)
+}
---
+++ backend/api/server/route.go
@@ -0,0 +1,33 @@
+package server
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/aldisatria12/terradiscover/handler"
+	"github.com/aldisatria12/terradiscover/repository"
+	"github.com/aldisatria12/terradiscover/service"
+	"github.com/gin-gonic/gin"
+)
+
+type Route struct {
+	userHandler *handler.UserHandler
+}
+
+func NewRoute(db *sql.DB) Route {
+	dataStore := repository.NewDataStore(db)
+	userRepository := repository.NewUserRepository(db)
+	userService := service.NewUserService(dataStore, userRepository)
+	userHandler := handler.NewUserHandler(userService)
+
+	return Route{
+		userHandler: &userHandler,
+	}
+}
+
+func (route *Route) SetRoutes() http.Handler {
+	r := gin.New()
+	r.POST("/user/login", route.userHandler.Login)
+
+	return r
+}
