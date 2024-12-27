6b007733632d8916961e4973b577b1710091aabf
aldisatria12 <aldisatria253@gmail.com>
2024-12-28 03:58:27 +0700

    feat: edit contact page without backend

---
+++ frontend/terradiscover/src/pages/EditContactPage/EditContactPage.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button, FormControl, TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
+import style from "./EditContactPage.module.css";
+import { Backdrop } from "../../component/UI/backdrop";
+
+export const EditContactPage: React.FC = () => {
+    const { register } = useForm();
+
+    return (
+        <div className={style.login_page}>
+            <Backdrop>
+                <h2>Create Contact</h2>
+                <form className={style.login_form} noValidate>
+                    <FormControl sx={{ m: 1, width: '40ch' }} variant="outlined">
+                        <TextField id="outlined-basic" label="Name" variant="outlined" {...register("name", {
+                            required: "Required",
+                        })} />
+                    </FormControl>
+                    <FormControl sx={{ m: 1, width: '40ch' }} variant="outlined">
+                        <TextField id="outlined-basic" label="Phone" variant="outlined" {...register("phone", {
+                            required: "Required",
+                        })} />
+                    </FormControl>
+                    <FormControl sx={{ m: 1, width: '40ch' }} variant="outlined">
+                        <TextField id="outlined-basic" label="Email" variant="outlined" {...register("email", {
+                            required: "Required",
+                        })} />
+                    </FormControl>
+                    <Button type="submit">
+                        Submit
+                    </Button>
+                </form>
+            </Backdrop>
+        </div >
+    )
+}
